08d315ce937816bd74d4e95a490b8c70e17e11c2
Felipe <felipezanluca@gmail.com>
2026-06-25 15:29:39 -0300

    Explore database structure and sample data

--- src/explore_database.py
+++ src/explore_database.py
@@ -1,3 +1,73 @@
-import database as db
+import pandas as pd
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
 
-db.get_connection()
+
+from database import get_connection
+
+# def explore_tables(tables):
+#     connection = get_connection()
+#
+#     try:
+#         for table_name in tables:
+#             print(f"\n" + "=" * 60)
+#             print(f"TABLE: {table_name}")
+#             print("=" * 60)
+#
+#             describe = pd.read_sql(
+#                 f"DESCRIBE {table_name}",
+#                 connection
+#             )
+#             print(describe)
+#             print("\n" + "=" * 60)
+#             print("SAMPLE:")
+#             sample = pd.read_sql(
+#                 f"SELECT * FROM {table_name} LIMIT 5",
+#             connection
+#             )
+#             print(sample)
+#
+#     finally:
+#         connection.close()
+
+def explore_table(table):
+    connection = get_connection()
+
+    try:
+        print("\n" + "=" * 60)
+        print(f"TABLE: {table}")
+        print("=" * 60)
+
+        describe = pd.read_sql(
+            f"DESCRIBE {table}",
+            connection
+        )
+        print(describe)
+
+        print("\n" + "=" * 60)
+        print("SAMPLE:")
+
+        sample = pd.read_sql(
+            f"SELECT * FROM {table} LIMIT 5",
+            connection
+        )
+        print(sample)
+
+    finally:
+        connection.close()
+
+def explore_tables(tables):
+    for table in tables:
+        explore_table(table)
+
+if __name__ == "__main__":
+
+    tables = [
+        "IMDB_movies",
+        "data_product",
+        "data_product_sales",
+        "data_store_cad",
+        "data_store_sales"
+    ]
+
+    explore_tables(tables)
---
+++ src/main.py
@@ -0,0 +1,15 @@
+from explore_database import explore_tables
+from explore_database import explore_table
+
+#For one exclusive table
+# explore_table("data_product")
+
+#For all tables
+# table = [
+#     "IMDB_movies",
+#     "data_product",
+#     "data_product_sales",
+#     "data_store_cad",
+#     "data_store_sales"
+# ]
+# explore_tables(table)
